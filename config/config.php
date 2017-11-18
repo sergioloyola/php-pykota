158f5a1bb9166a57299b8449d4348a8ecbbcbb47
Sergio Loyola <sergio.d.loyola@gmail.com>
2017-11-18 14:28:58 -0300

    Sergio
    Ultima actualización

--- config/config.php
+++ config/config.php
@@ -9,4 +9,5 @@ $basedatos = "pykota";
 $conexion=mysql_connect ($servidor, $usuario, $clave) or die ('problema conectando porque :' . mysql_error());
 mysql_select_db($basedatos, $conexion); 
 $estilo = "template/kion/style.css";
+$estilo_login = "template/kion/login.css";
 ?>
